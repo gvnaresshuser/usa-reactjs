7243e6c0df19c2e6196af9cd47a1bcaf483f60a8
gvnaresshuser <gvnaressh@gmail.com>
2026-07-01 15:34:29 +0530

    more additions

--- my-react-app/src - M1-Lesson-1-ES6/App.jsx
+++ my-react-app/src - M1-Lesson-1-ES6/App.jsx
@@ -49,55 +49,76 @@ function App() {
   // JSX (ReactNode) as a variable
   const welcomeMessage = <h2>Welcome to React JS!</h2>;
 
+  //------------------------
+  function getName(name) {
+    return name;
+  }
+  const a = true;
+  const b = false;
+
+  // If first value is true, second value is not executed
+  const logicalOr1 = a || getName("Naressh Gudimetla");
+  // If first value is false, second value is executed
+  const logicalOr2 = b || getName("Naressh Gudimetla");
+
+  // If first value is true, second value is executed
+  const logicalAnd1 = a && getName("Naressh Gudimetla");
+  // If first value is false, second value is not executed
+  const logicalAnd2 = b && getName("Naressh Gudimetla");
+  //------------------------
+
   return (
     <div style={{ padding: "20px", fontFamily: "Arial" }}>
       <h1>React JS Data Types Demo</h1>
-
       {/* String */}
       <p>
         <strong>Name:</strong> {name}
       </p>
-
       {/* Number */}
       <p>
         <strong>Age:</strong> {age}
       </p>
-
       <p>
         <strong>isLoggedIn:</strong> {isLoggedIn}
       </p>
-
       <p>
         <strong>isLoggedIn:</strong> {String(isLoggedIn)}
       </p>
       <p>
         <strong>isLoggedIn:</strong> {isLoggedIn.toString()}
       </p>
-
       {/* Boolean (used in conditional rendering) */}
       <p>
         <strong>Status:</strong> {isLoggedIn ? "Logged In" : "Guest"}
       </p>
-
       {/* Array (using map) */}
       <p>
         <strong>Hobbies:</strong>
       </p>
-
       {/* <ul>
         {hobbies.map((hobby, index) => (
           <li key={index}>{hobby}</li>
         ))}
       </ul> */}
-      <ol>
+      {/* <ol>
         {hobbies.map(
           //loop run
           (hobby, index) => (
             <li key={index}>{hobby}</li>
           )
         )}
-      </ol>
-
+      </ol> */}
+      {/* 
+      ASI = JavaScript Automatic Semicolon Insertion (ASI) issue. 
+      return <div key={index}>
+      */}
+      {/* <div style={{ listStyleType: "none", padding: 0 }}>
+        {hobbies.map((hobby, index) => {
+          return <div key={index}>
+              {index + 1}.{hobby}
+            </div>;          
+        })}
+      </div> */}
       <p>
         <strong>Users:</strong>
       </p>
@@ -108,23 +129,42 @@ function App() {
           </li>
         ))}
       </ul>
-
+      {/* BETTER - using key property WITH unique value (user.id) instead of index 
+      - to avoid potential issues with reordering or filtering the list. */}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.id} - {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
       {/* Object */}
       <p>
         <strong>User Email:</strong> {user.password}
       </p>
-
       {/* Function */}
       <button onClick={handleClick}>Click Me</button>
       <button onClick={() => handleClick()}>Click Me 1</button>
       <button onClick={() => handleClickParams(20000)}>Click Me 2</button>
-
+      {/* WILL NOT WORK */}
       {/* <button onClick={handleClickParams(200)}>Click Me</button> */}
 
       {/* JSX element as variable */}
       <div>{welcomeMessage}</div>
-
+      {/* Logical AND operator */}
       {isLoggedIn && <p>Welcome back, {name}!</p>}
+
+      <div className="cardx">
+        <h2>Logical AND (&&)</h2>
+        <p>{logicalAnd1}</p>
+        <p>{String(logicalAnd2)}</p>
+      </div>
+
+      <div className="cardx">
+        <h2>Logical OR (||)</h2>
+        <p>{String(logicalOr1)}</p>
+        <p>{logicalOr2}</p>
+      </div>
 
       {/* External function passed */}
       <button onClick={greetUser} style={{ marginTop: "10px" }}>
