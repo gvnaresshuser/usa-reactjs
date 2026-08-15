24500164ad09dc00a17b5940b19dbbad20719e93
gvnaresshuser <gvnaressh@gmail.com>
2026-08-15 08:40:16 +0530

    char counter

--- my-react-app/src -M3-Lesson-4-useState and useEffect-IMP/App.jsx
+++ my-react-app/src -M3-Lesson-4-useState and useEffect-IMP/App.jsx
@@ -1,59 +1,26 @@
-import { useState, useEffect } from "react";
 import "./App.css";
+import Counter from "./Counter"; //useState
+import Timer from "./Timer"; //useEffect
+
+import CounterMessage from "./CounterMessage"; //Assignment on useState
+import LiveCharCounter from "./LiveCharCounter"; //Assignment on useEffect
 
 function App() {
-  console.log("APP");
-  const [data, setData] = useState([]);
-  const [count, setCount] = useState(0);
-
-  function increment() {
-    //setCount((count)=>count + 1);
-    setCount(11);
-    //setCount((prevCount) => prevCount + 1);
-    console.log("Inside increment:" + count);
-  }
-
-  const fetchData = async () => {
-    console.log("FETCH DATA");
-    try {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/users",
-      );
-      console.log(response);
-      if (!response.ok) throw new Error("Network response was not ok");
-      const datax = await response.json();
-      console.log(datax);
-      setData(datax);
-      console.log("SET DATA");
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-  //Runs infinitely if we call fetchData() directly inside your component:
-  //fetchData();
-
-/*   useEffect(() => {
-    console.log("USE EFFECT");
-    fetchData();
-  }, []);
- */
-
-  useEffect(() => {
-    console.log("USE EFFECT");
-    increment();
-  }, [count]);
-
   return (
     <>
-      <h2>Fetched Data</h2>
-      <ul>
-        {console.log("RENDERING")}
-        {data.map((user) => (
-          <li key={user.id}>
-            {user.name} - {user.email}
-          </li>
-        ))}
-      </ul>
+      <div className="App">
+        {/*  USE STATE DEMO */}
+        {/* <Counter /> */}
+
+        {/* USE EFFECT DEMO */}
+        {/* <Timer /> */}
+
+        {/* ASSIGNMENT ON USE STATE */}
+        {/* <CounterMessage /> */}
+
+        {/* ASSIGNMENT ON USE EFFECT */}
+        <LiveCharCounter />
+      </div>
     </>
   );
 }
